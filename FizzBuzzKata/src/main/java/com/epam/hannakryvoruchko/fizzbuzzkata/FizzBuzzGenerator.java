8c1f7148ed91f4eb8fdb9488a5d6af9690454917
Hanna_Kryvoruchko1 <Hanna_Kryvoruchko@epam.com>
2015-05-25 18:55:10 +0300

    added simple test

--- FizzBuzzKata/src/main/java/com/epam/hannakryvoruchko/fizzbuzzkata/FizzBuzzGenerator.java
+++ FizzBuzzKata/src/main/java/com/epam/hannakryvoruchko/fizzbuzzkata/FizzBuzzGenerator.java
@@ -4,4 +4,8 @@ package com.epam.hannakryvoruchko.fizzbuzzkata;
  * Created by Hanna_Kryvoruchko1 on 5/25/2015.
  */
 public class FizzBuzzGenerator {
+
+    public String generate(int i) {
+        return null;
+    }
 }
--- FizzBuzzKata/src/test/java/com/epam/hannakryvoruchko/fizzbuzzkata/FizzBuzzGeneratorTest.java
+++ FizzBuzzKata/src/test/java/com/epam/hannakryvoruchko/fizzbuzzkata/FizzBuzzGeneratorTest.java
@@ -1,7 +1,24 @@
 package com.epam.hannakryvoruchko.fizzbuzzkata;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  * Created by Hanna_Kryvoruchko1 on 5/25/2015.
  */
 public class FizzBuzzGeneratorTest {
+
+    private FizzBuzzGenerator fizzBuzzGenerator;
+
+    @Before
+    public void setUp() {
+        fizzBuzzGenerator = new FizzBuzzGenerator();
+    }
+
+    @Test
+    public void testGenerate(){
+        fizzBuzzGenerator.generate(0);
+    }
+
 }
