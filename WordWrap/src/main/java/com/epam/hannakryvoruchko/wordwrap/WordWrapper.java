e796f031e3341fb10be113c5e5f04728bef1801e
Hanna_Kryvoruchko1 <Hanna_Kryvoruchko@epam.com>
2015-05-27 17:11:40 +0300

    ---

---
+++ WordWrap/src/main/java/com/epam/hannakryvoruchko/wordwrap/WordWrapper.java
@@ -0,0 +1,10 @@
+package com.epam.hannakryvoruchko.wordwrap;
+
+/**
+ * Created by Hanna_Kryvoruchko1 on 5/27/2015.
+ */
+public class WordWrapper {
+    public static String wrap(String sentence, int columnLength) {
+        return null;
+    }
+}
--- WordWrap/src/test/java/com/epam/hannakryvoruchko/wordwrap/WordWrapTest.java
+++ WordWrap/src/test/java/com/epam/hannakryvoruchko/wordwrap/WordWrapTest.java
@@ -2,13 +2,16 @@ package com.epam.hannakryvoruchko.wordwrap;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Hanna_Kryvoruchko1 on 5/27/2015.
  */
 public class WordWrapTest {
 
     @Test
-    public void doNothing() {
+    public void noWrap() {
+        assertEquals("word", WordWrapper.wrap("word", 4));
 
     }
 
