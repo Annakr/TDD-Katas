377e93334748db13b50705e33f6a4560ecdbee83
Hanna_Kryvoruchko1 <Hanna_Kryvoruchko@epam.com>
2015-05-27 17:06:27 +0300

    ---

---
+++ WordWrap/src/test/java/com/epam/hannakryvoruchko/wordwrap/WordWrapTest.java
@@ -0,0 +1,15 @@
+package com.epam.hannakryvoruchko.wordwrap;
+
+import org.junit.Test;
+
+/**
+ * Created by Hanna_Kryvoruchko1 on 5/27/2015.
+ */
+public class WordWrapTest {
+
+    @Test
+    public void doNothing() {
+
+    }
+
+}
