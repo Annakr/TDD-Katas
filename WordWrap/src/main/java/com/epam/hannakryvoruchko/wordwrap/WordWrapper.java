785967a20b50743375fdbd5018c9366088581784
Hanna_Kryvoruchko1 <Hanna_Kryvoruchko@epam.com>
2015-05-28 12:32:53 +0300

    ---

--- WordWrap/src/main/java/com/epam/hannakryvoruchko/wordwrap/WordWrapper.java
+++ WordWrap/src/main/java/com/epam/hannakryvoruchko/wordwrap/WordWrapper.java
@@ -4,9 +4,15 @@ package com.epam.hannakryvoruchko.wordwrap;
  * Created by Hanna_Kryvoruchko1 on 5/27/2015.
  */
 public class WordWrapper {
+
+    public static final String LINE_SEPARATOR = "\n";
+
     public static String wrap(String sentence, int columnLength) {
-        if(sentence.length() > columnLength)
-            return sentence.substring(0, columnLength) + "\n" + wrap(sentence.substring(columnLength), columnLength);
+        if(sentence.length() > columnLength) {
+            String firstPartOfSentence = sentence.substring(0, columnLength);
+            String restOfSentence = sentence.substring(columnLength);
+            return firstPartOfSentence + LINE_SEPARATOR + wrap(restOfSentence, columnLength);
+        }
         else
             return sentence;
     }
