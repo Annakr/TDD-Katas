fcf8936f7633f14a6648c6e1fa9b4d24927a1cf3
Hanna_Kryvoruchko1 <Hanna_Kryvoruchko@epam.com>
2015-05-28 12:46:21 +0300

    ---

--- WordWrap/src/main/java/com/epam/hannakryvoruchko/wordwrap/WordWrapper.java
+++ WordWrap/src/main/java/com/epam/hannakryvoruchko/wordwrap/WordWrapper.java
@@ -9,7 +9,7 @@ public class WordWrapper {
 
     public static String wrap(String sentence, int columnLength) {
         if(sentence.length() > columnLength) {
-            String firstPartOfSentence = sentence.substring(0, columnLength);
+            String firstPartOfSentence = sentence.substring(0, columnLength).trim();
             String restOfSentence = sentence.substring(columnLength);
             return firstPartOfSentence + LINE_SEPARATOR + wrap(restOfSentence, columnLength);
         } else
